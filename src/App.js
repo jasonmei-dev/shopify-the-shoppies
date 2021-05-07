5e012fcde9dbde847cb939e97f96eb149092424a
jmei403 <jason.mei403@gmail.com>
2021-05-07 18:03:57 -0400

    Alert component & show alert on max nominations

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import SearchBar from './components/SearchBar';
 import Movies from './components/Movies';
 import Nominations from './components/Nominations';
+import Alert from './components/Alert';
 
 import axios from 'axios';
 import './App.css';
@@ -10,10 +11,12 @@ const App = () => {
   const [text, setText] = useState("");
   const [movies, setMovies] = useState([]);
   const [nominations, setNominations] = useState([]);
+  const [alert, setAlert] = useState(null);
 
   useEffect(() => {
     searchMovies(text);
-  }, [text]);
+    maxedNominations();
+  }, [text, nominations]);
 
   const searchMovies = async (text) => {
     const res = await axios.get(`https://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&s=${text}`);
@@ -21,11 +24,17 @@ const App = () => {
     setMovies(res.data.Search || []);
   }
 
+  const maxedNominations = () => {
+    if (nominations.length === 5) {
+      showAlert("You reached the max number of nominations!", "success");
+    }
+  }
+
   const addNomination = movie => {
     if (nominations.length < 5) {
       setNominations([...nominations, movie]);
     } else {
-      alert("You already have 5 nominations!");
+      showAlert("You already have 5 nominations!", "danger");
     }
   }
 
@@ -35,9 +44,15 @@ const App = () => {
 
   const isNominated = movie => !!nominations.find(nomination => nomination.imdbID === movie.imdbID);
 
+  const showAlert = (msg, type) => {
+    setAlert({ msg, type });
+    setTimeout(() => setAlert(null), 3000);
+  }
+
   return (
     <div className="App">
       <h1>The Shoppies</h1>
+      <Alert alert={alert} />
       <SearchBar text={text} onChange={(e) => setText(e.target.value)} resetText={() => setText("")} />
       <Movies movies={movies} text={text} addNomination={addNomination} isNominated={isNominated} />
       <Nominations nominations={nominations} removeNomination={removeNomination} />
