9dd86df77d0d14bd8cf007e8bc4246655f173709
jmei403 <jason.mei403@gmail.com>
2021-05-07 17:44:02 -0400

    Move results header

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,20 +2,18 @@ import React from 'react'
 
 const Movies = ({ movies, text, addNomination, isNominated }) => {
   return (
-    movies.length > 0 && (
-      <div className="Movies">
-        <h2>Results for "{text}"</h2>
-        { movies.length > 0 &&
-          <ul>
-            {movies.map(movie => (
-              <li key={movie.imdbID}>
-                {movie.Title} ({movie.Year}) {" "} <button onClick={() => addNomination(movie)} disabled={isNominated(movie)}>Nominate</button>
-              </li>
-            ))}
-          </ul>
-        }
-      </div>
-    )
+    <div className="Movies">
+      <h2>Results for "{text}"</h2>
+      { movies.length > 0 &&
+        <ul>
+          {movies.map(movie => (
+            <li key={movie.imdbID}>
+              {movie.Title} ({movie.Year}) {" "} <button onClick={() => addNomination(movie)} disabled={isNominated(movie)}>Nominate</button>
+            </li>
+          ))}
+        </ul>
+      }
+    </div>
   )
 }
 
